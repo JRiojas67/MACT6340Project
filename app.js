441b6bbbc5d69d97f640b2080df741c3d2549e1d
Javier Riojas <javierriojas@mac.attlocal.net>
2025-09-27 22:31:25 -0500

    Reorganize index.ejs page and installed ejs

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ dotenv.config();
 
 const app = express();
 const port = 3000;
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
 
 app.post("/mail", async (req, res) => {
   await utils
