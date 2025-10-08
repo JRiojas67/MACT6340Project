6bcf1048ede6b7bb4b7f9e2d53f40f22d58468f7
Javier Riojas <javierriojas@mac.attlocal.net>
2025-10-07 19:28:05 -0500

    Updated index.js file and created a connet wallet

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,37 @@
 "use strict";
 
-(() =>{})() ;
+
+
+(() =>{
+    let userAddress = null;
+    let connect = document.querySelector("#wallet-connect");
+
+    connectWallet();
+
+connect.addEventListener("click", async () => {
+    connectWallet();
+});
+
+    async function connectWallet() {
+        await window.ethereum
+        .request({ method: "eth_requestAccounts"})
+        .then((data) => {
+            userAddress = data[0];
+            let walletString =
+                userAddress.substring(0, 5) + "..." + userAddress.substring(38, 42);
+                connect.innerHTML = walletString;
+                return userAddress;
+        })
+    .catch((error) => {
+        if (error.code === 4001) {
+            //EIP-1193 userRecjectedRequest error
+            //If this happens, the user rejected the connection request.
+            console.log("Please connect a wallet");
+        }else{
+            console.error(error);
+        }
+        
+    });
+}
+
+})() ;
